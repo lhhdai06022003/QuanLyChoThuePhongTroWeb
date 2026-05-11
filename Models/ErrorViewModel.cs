104404b9e7c1e5867d43224fddbe1fd9aac0eef2
lhhdai06022003 <lhhd06022003@gmail.com>
2026-05-11 19:20:58 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyChoThuePhongTroWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
