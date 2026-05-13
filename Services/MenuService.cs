4b94db1f518778f4cf7e187ccb2ada22fc069af6
lhhdai06022003 <lhhd06022003@gmail.com>
2026-05-14 00:01:34 +0700

    layout menu và add thư viện

---
+++ Services/MenuService.cs
@@ -0,0 +1,40 @@
+﻿using QuanLyChoThuePhongTroWeb.Models.ModelsOther;
+
+namespace QuanLyChoThuePhongTroWeb.Services
+{
+    public class MenuService
+    {
+        public static List<MenuItem> GetMenu()
+        {
+            return new List<MenuItem>
+        {
+            new MenuItem
+            {
+                Name = "Dashboard",
+                Icon = "fas fa-home",
+                Url = "/Home/Index"
+            },
+            new MenuItem
+            {
+                Name = "Quản lý người dùng",
+                Icon = "fas fa-users",
+                Children = new List<MenuItem>
+                {
+                    new MenuItem { Name = "Danh sách", Url = "/User/Index" },
+                    new MenuItem { Name = "Thêm mới", Url = "/User/Create" }
+                }
+            },
+            new MenuItem
+            {
+                Name = "Quản lý câu hỏi",
+                Icon = "fas fa-question",
+                Children = new List<MenuItem>
+                {
+                    new MenuItem { Name = "Chủ đề", Url = "/Topic/Index" },
+                    new MenuItem { Name = "Câu hỏi", Url = "/Question/Index" }
+                }
+            }
+        };
+        }
+    }
+}
